d543a5324585bf128310841a173fc5d119b25879
Daria Malina <gluto68@gmail.com>
2022-05-12 16:41:15 +0300

    feat: add custom logo

--- src/Components/footer/Footer.component.tsx
+++ src/Components/footer/Footer.component.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import { FooterBlock, FooterLogoGithub } from './styled';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import logoDima from '../../assets/svg/logoDima.svg';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import logoJ from '../../assets/svg/logoJ.svg';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import logoDasha from '../../assets/svg/logoDasha.svg';
 
-const LINK_LOGO_GITHUB = 'https://www.svgrepo.com/show/359980/github.svg';
+const LINK_LOGO_GITHUB = [logoDima, logoJ, logoDasha];
 const LINK_GITHUB_PAGES = [
   'https://github.com/Dmitriy-hello-world',
   'https://github.com/Evgeniy37529',
-  'https://github.com/DariaMalina',
+  'https://github.com/DariaMalina'
 ];
 
 const Footer = () => {
   return (
     <FooterBlock>
-      {LINK_GITHUB_PAGES.map((el) => {
+      {LINK_GITHUB_PAGES.map((el, index) => {
         return (
           <a href={el} key={el}>
-            <FooterLogoGithub src={LINK_LOGO_GITHUB} alt="logo" />
+            <FooterLogoGithub src={LINK_LOGO_GITHUB[index]} alt="logo" />
           </a>
         );
       })}
