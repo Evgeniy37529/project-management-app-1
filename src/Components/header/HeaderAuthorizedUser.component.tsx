3fbf4d3f8b6bcc8a1d1265fbb4c2a5653ba6a998
Daria Malina <gluto68@gmail.com>
2022-05-12 20:04:30 +0300

    feat: creating a temporary entry

--- src/Components/header/HeaderAuthorizedUser.component.tsx
+++ src/Components/header/HeaderAuthorizedUser.component.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { Button, Switch, PageHeader } from 'antd';
 import { theme } from '../../utils/theme';
 import { PlusCircleOutlined, SettingOutlined, ExportOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
 
 const HeaderAuthorisingUser = () => {
+  const navigate = useNavigate();
+  const exit = () => {
+    localStorage.clear();
+    navigate('/');
+  };
   return (
     <PageHeader
       className="site-page-header-responsive"
@@ -15,7 +21,7 @@ const HeaderAuthorisingUser = () => {
         <Button key="login" icon={<SettingOutlined />}>
           Edit profile
         </Button>,
-        <Button key="sign-up" danger icon={<ExportOutlined />}>
+        <Button key="sign-up" danger icon={<ExportOutlined />} onClick={exit}>
           Sign Out
         </Button>,
         <Switch checkedChildren="en" unCheckedChildren="ru" defaultChecked key="language" />
