3ff33c4580160a82dc69124f0cfaff64d6264ef7
Daria Malina <gluto68@gmail.com>
2022-05-12 16:56:30 +0300

    feat: add logo link

--- src/Components/footer/Footer.component.tsx
+++ src/Components/footer/Footer.component.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import { FooterBlock, FooterLogoGithub } from './styled';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import logoDima from '../../assets/svg/logoDima.svg';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import logoJ from '../../assets/svg/logoJ.svg';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import logoDasha from '../../assets/svg/logoDasha.svg';
 
-const LINK_LOGO_GITHUB = [logoDima, logoJ, logoDasha];
+const LINK_LOGO_GITHUB = [
+  'http://svgur.com/i/h50.svg',
+  'http://svgur.com/i/h4K.svg',
+  'http://svgur.com/i/h5q.svg'
+];
 const LINK_GITHUB_PAGES = [
   'https://github.com/Dmitriy-hello-world',
   'https://github.com/Evgeniy37529',
