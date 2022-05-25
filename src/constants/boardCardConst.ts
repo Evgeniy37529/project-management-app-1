91dae6e5ca24e7c1be00a3500955ad3749c50e61
Daria Malina <gluto68@gmail.com>
2022-05-25 21:20:48 +0300

    fix: add removed parts of code

--- src/constants/boardCardConst.ts
+++ src/constants/boardCardConst.ts
@@ -1,19 +1,40 @@
-export const exampleCardObj = {
-  title: 'It is title',
-  info: [
-    'something info info info info info info info info',
-    'something long long long long long long long long long long info info info info info info info info',
-    'something very  very very  very  very very very very long long long long long long long long long long info info info info info info info info',
-    'something info info info info info info info info',
-    'something long long long long long long long long long long info info info info info info info info',
-    'something very  very very  very  very very very very long long long long long long long long long long info info info info info info info info',
-    'something info info info info info info info info',
-    'something long long long long long long long long long long info info info info info info info info',
-    'something very  very very  very  very very very very long long long long long long long long long long info info info info info info info info'
-  ]
-};
+export const initialData: {
+  tasks: { [key: string]: { id: string; content: string } };
+  columns: { [key: string]: { id: string; title: string; taskIds: string[] } };
+  columnOrder: string[];
+} = {
+  tasks: {
+    'task-1': { id: 'task-1', content: 'Take out the garbage' },
+    'task-2': { id: 'task-2', content: 'Watch my favorite show' },
+    'task-3': { id: 'task-3', content: 'Charge my phone' },
+    'task-4': { id: 'task-4', content: 'Cook dinner' },
+    'task-5': { id: 'task-5', content: 'Take out the garbage' },
+    'task-6': { id: 'task-6', content: 'Watch my favorite show' },
+    'task-7': { id: 'task-7', content: 'Charge my phone' },
+    'task-8': { id: 'task-8', content: 'Cook dinner' }
+  },
+  columns: {
+    'column-1': {
+      id: 'column-1',
+      title: 'To do',
+      taskIds: ['task-1', 'task-2']
+    },
+    'column-2': {
+      id: 'column-2',
+      title: 'To do',
+      taskIds: ['task-3', 'task-4']
+    },
+    'column-3': {
+      id: 'column-3',
+      title: 'To do',
+      taskIds: ['task-5', 'task-6']
+    },
+    'column-4': {
+      id: 'column-4',
+      title: 'To do',
+      taskIds: ['task-7', 'task-8']
+    }
+  },
 
-export const exampleCardObjSecond = {
-  title: 'It is title',
-  info: []
+  columnOrder: ['column-1', 'column-2', 'column-3', 'column-4']
 };
